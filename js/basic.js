f39ce58aeac91d61a2425f148352d8943dff3d46
enkikkeli <onni.riikonen@student.lut.fi>
2024-11-04 18:46:16 +0200

    1

--- js/basic.js
+++ js/basic.js
@@ -1,40 +1,42 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const container = document.createElement("div");
-    document.body.appendChild(container);
+const wikiItem = document.createElement("div");
+wikiItem.classList.add("wiki-item");
+document.body.appendChild(wikiItem);
 
-    const data = [
-        { title: "Dog1", img: "dog/dog1.jpeg", text: "text" },
-        { title: "Dog2", img: "dog/dog2.jpeg", text: "text" },
-        { title: "Dog3", img: "dog/dog3.jpeg", text: "text" },
-        { title: "Dog4", img: "dog/dog4.jpeg", text: "text" },
-        { title: "Dog5", img: "dog/dog5.jpeg", text: "text" }
+const data = [
+    { title: "Dog1", img: "dog/dog1.jpeg", text: "text" },
+    { title: "Dog2", img: "dog/dog2.jpeg", text: "text" },
+    { title: "Dog3", img: "dog/dog3.jpeg", text: "text" },
+    { title: "Dog4", img: "dog/dog4.jpeg", text: "text" },
+    { title: "Dog5", img: "dog/dog5.jpeg", text: "text" }
 
-    ];
+];
 
-    function create({ title, img, text }) {
-        const wikiItem = document.createElement("div");
+function create({ title, img, text }) {
+    
 
-        const header = document.createElement("h1");
-        header.textContent = title;
+    const header = document.createElement("h1");
+    header.classList.add("wiki-header");
+    header.textContent = title;
 
-        const content = document.createElement("div");
+    const wikiContent = document.createElement("div");
+    wikiContent.classList.add("wiki-content");
 
-        const paragraph = document.createElement("p");
-        paragraph.textContent = text;
+    const wikiText = document.createElement("p");
+    wikiText.classList.add("wiki-text");
+    wikiText.textContent = text;
 
-        const imgContainer = document.createElement("div");
+    const imgContainer = document.createElement("div");
+    imgContainer.classList.add("img-container");
 
-        const image = document.createElement("img");
-        image.src = img
+    const image = document.createElement("img");
+    image.classList.add("wiki-img");
+    image.src = img
 
-        imgContainer.appendChild(image);
-        content.appendChild(paragraph);
-        content.appendChild(imgContainer);
-        wikiItem.appendChild(header);
-        wikiItem.appendChild(content);
+    imgContainer.appendChild(image);
+    wikiContent.appendChild(wikiText);
+    wikiContent.appendChild(imgContainer);
+    wikiItem.appendChild(header);
+    wikiItem.appendChild(wikiContent);
+}
 
-        container.appendChild(wikiItem);
-    }
-
-    data.forEach(create);
-});
+data.forEach(create);
